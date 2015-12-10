31a27e6016fea2c8c3ba8a3e80d4b392e9e7953d
Alex Moore-Niemi <moore.niemi@gmail.com>
2015-12-09 22:34:06 -0500

    unquote error

--- build.fsx
+++ build.fsx
@@ -27,18 +27,29 @@ Target "Source.dll" (fun _ ->
                     FscTarget = Library })
 )
 
+let dependencies = [ "packages/System.Runtime/lib/netcore50/System.Runtime.dll";
+                    "packages/System.Private.Uri/lib/netcore50/System.Private.Uri.dll";
+                    "packages/System.Diagnostics.Debug/lib/netcore50/System.Diagnostics.Debug.dll";
+                    "packages/System.Reflection/lib/netcore50/System.Reflection.dll";
+                    "packages/System.Threading.Tasks/lib/netcore50/System.Threading.Tasks.dll";
+                    "packages/System.Collections/lib/netcore50/System.Collections.dll";
+                    "packages/System.Linq/ref/netcore50/System.Linq.dll";
+                    "packages/System.Reflection.Extensions/lib/netcore50/System.Reflection.Extensions.dll";
+                    "packages/System.Globalization/lib/netcore50/System.Globalization.dll";
+                    "packages/System.Runtime.Extensions/lib/netcore50/System.Runtime.Extensions.dll";
+                    "packages/xunit.runner.console/tools/xunit.abstractions.dll";
+                    "packages/xunit.extensibility.execution/lib/net45/xunit.execution.desktop.dll";
+                    "packages/xunit.extensibility.core/lib/dotnet/xunit.core.dll";
+                    "packages/Unquote/lib/net40/Unquote.dll";
+                    "build/Source.dll" ]
+
 Target "Tests.dll" (fun _ ->
   ["src/spec/dayOneSpec.fs"] //!! "src/spec/*.fs"
   |> Fsc (fun p ->
             { p with Output = "./build/test/Tests.dll"
                      FscTarget = Library
-                     References =
-                      [ "packages/System.Runtime/lib/netcore50/System.Runtime.dll"
-                        "packages/xunit.runner.console/tools/xunit.abstractions.dll"
-                        "packages/xunit.extensibility.execution/lib/net45/xunit.execution.desktop.dll"
-                        "packages/xunit.extensibility.core/lib/dotnet/xunit.core.dll"
-                        "packages/Unquote/lib/net40/Unquote.dll"
-                        "build/Source.dll" ] }) 
+                     OtherParams = ["--standalone"]
+                     References = dependencies })
 )
 
 // define test dlls
@@ -46,8 +57,8 @@ let testDlls = !! (testDir + "/Tests.dll")
 
 Target "xUnitTest" (fun _ ->
     testDlls
-        |> xUnit2 (fun p -> 
-            {p with 
+        |> xUnit2 (fun p ->
+            {p with
                 ShadowCopy = false })
 )
 
