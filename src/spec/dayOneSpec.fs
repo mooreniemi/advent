cec5eebe987e454c8201b8c0305b5ba261ecda9f
Alex Moore-Niemi <moore.niemi@gmail.com>
2015-12-10 23:11:45 -0500

    sample test cases covered

--- src/spec/dayOneSpec.fs
+++ src/spec/dayOneSpec.fs
@@ -20,3 +20,27 @@ module DayOneSpec =
   [<Test>]
     let ``counts more parens`` () =
       santaClimbing "()()" |> should equal 0
+
+  [<Test>]
+    let ``more than zero`` () =
+      santaClimbing "(((" |> should equal 3
+
+  [<Test>]
+    let ``more than zero either way`` () =
+      santaClimbing "(()(()(" |> should equal 3
+
+  [<Test>]
+    let ``does the math`` () =
+      santaClimbing "))(((((" |> should equal 3
+
+  [<Test>]
+    let ``handles negatives`` () =
+      santaClimbing "())" |> should equal -1
+
+  [<Test>]
+    let ``handles negatives again`` () =
+      santaClimbing ")))" |> should equal -3
+
+  [<Test>]
+    let ``handles negatives again again`` () =
+      santaClimbing ")())())" |> should equal -3
