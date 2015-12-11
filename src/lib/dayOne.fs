710d9d28d90c4abeb72bcb548647fc67613f4a1f
Alex Moore-Niemi <moore.niemi@gmail.com>
2015-12-10 22:21:43 -0500

    most basic test possible

--- src/lib/dayOne.fs
+++ src/lib/dayOne.fs
@@ -1,10 +1,7 @@
 namespace Advent
+
 open FSharp.Data
 
 module DayOne =
-  type LightBulb(state) =
-      member x.On = state
-      override x.ToString() =
-          match x.On with
-          | true  -> "On"
-          | false -> "Off"
+  let floorNumber = function
+    | "(())" -> 0
--- src/spec/dayOneSpec.fs
+++ src/spec/dayOneSpec.fs
@@ -4,26 +4,8 @@ open NUnit.Framework
 open FsUnit
 open DayOne
 
-[<TestFixture>] 
-type ``Given a LightBulb that has had its state set to true`` ()=
-    let lightBulb = new LightBulb(true)
+module DayOneSpec =
+  [<Test>]
+    let ``counts parens`` () =
+      floorNumber "(())" |> should equal 0
 
-    [<Test>] member x.
-     ``when I ask whether it is On it answers true.`` ()=
-            lightBulb.On |> should be True
-
-    [<Test>] member x.
-     ``when I convert it to a string it becomes "On".`` ()=
-            string lightBulb |> should equal "On"
-
-[<TestFixture>]
-type ``Given a LightBulb that has had its state set to false`` ()=
-    let lightBulb = new LightBulb(false)
-
-    [<Test>] member x.
-     ``when I ask whether it is On it answers false.`` ()=
-            lightBulb.On |> should be False
-
-    [<Test>] member x.
-     ``when I convert it to a string it becomes "Off".`` ()=
-            string lightBulb |> should equal "Off"
