dec654e26ebf9de08bd77ec92ad10eb9f61294dc
Alex Moore-Niemi <moore.niemi@gmail.com>
2015-12-10 23:24:56 -0500

    first answer :grin:

--- src/lib/dayOne.fs
+++ src/lib/dayOne.fs
@@ -1,6 +1,10 @@
 namespace Advent
 
+open System.IO
+
 module DayOne =
+  let getInstructions filePath = System.IO.File.ReadAllText(filePath)
+
   let floorNumber = function
     | '(' -> 1
     | ')' -> -1
@@ -8,3 +12,7 @@ module DayOne =
 
   let santaClimbing (stairs : string) =
     List.ofSeq stairs |> List.map floorNumber |> List.sum
+
+  let whatFloor =
+    santaClimbing (getInstructions "data/day1.txt")
+
